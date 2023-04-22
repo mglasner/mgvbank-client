7a3fcb40039af3f1b5db90d862726f24c40fa7f5
mgv <mglasner10@gmail.com>
2023-04-21 21:16:46 -0400

    logout

--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,34 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import { onAuthStateChanged, getAuth, signOut } from "firebase/auth";
+import firebase_app from "@/firebase/config";
 
+const auth = getAuth(firebase_app);
 export default function Navbar() {
+  const [user, setUser] = useState(null);
+
   const router = useRouter();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+  }, [user]);
+
+  const handleLogOut = async function () {
+    try {
+      let result = await signOut(auth);
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <div className="container">
@@ -31,7 +56,7 @@ export default function Navbar() {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
+              <li className={`nav-item ${user ? "" : "d-none"}`}>
                 <Link
                   className={`nav-link ${
                     router.pathname === "/deposit" ? "active" : ""
@@ -41,7 +66,7 @@ export default function Navbar() {
                   Deposit
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className={`nav-item ${user ? "" : "d-none"}`}>
                 <Link
                   className={`nav-link ${
                     router.pathname === "/withdraw" ? "active" : ""
@@ -53,7 +78,7 @@ export default function Navbar() {
               </li>
             </ul>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li className="nav-item">
+              <li className={`nav-item ${user ? "d-none" : ""}`}>
                 <Link
                   className={`nav-link ${
                     router.pathname === "/login" ? "active" : ""
@@ -69,7 +94,7 @@ export default function Navbar() {
                   />
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className={`nav-item ${user ? "d-none" : ""}`}>
                 <Link
                   className={`nav-link ${
                     router.pathname === "/signup" ? "active" : ""
@@ -79,6 +104,22 @@ export default function Navbar() {
                   Signup
                   <Image
                     src="/images/sign-up.svg"
+                    height={20}
+                    width={20}
+                    alt="bank logo"
+                  />
+                </Link>
+              </li>
+              <li className={`nav-item ${user ? "" : "d-none"}`}>
+                <Link className="nav-link" href="#">
+                  {user ? user.email : ""}
+                </Link>
+              </li>
+              <li className={`nav-item ${user ? "" : "d-none"}`}>
+                <Link className="nav-link" href="#" onClick={handleLogOut}>
+                  Logout
+                  <Image
+                    src="/images/log-out.svg"
                     height={20}
                     width={20}
                     alt="bank logo"
