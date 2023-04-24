d462ba398ec41821526f585bfbb46995c8279759
mgv <mglasner10@gmail.com>
2023-04-23 22:44:01 -0400

    component level style for card

--- components/card.js
+++ components/card.js
@@ -1,3 +1,5 @@
+import styles from "./card.module.css";
+
 export default function Card(props) {
   const classes = function () {
     const bg = props.bgcolor ? " bg-" + props.bgcolor : "bg-dark";
@@ -6,7 +8,7 @@ export default function Card(props) {
   };
 
   return (
-    <div className={`mt-5 ${classes()}`}>
+    <div className={`mt-5 ${classes()} ${styles.card}`}>
       <div className="card-header">{props.header}</div>
       <div className="card-body">
         {props.title && <h5 className="card-title">{props.title}</h5>}
