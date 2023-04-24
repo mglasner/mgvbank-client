46a83a9f40472b02d2c00204df501740cf2cd564
mgv <mglasner10@gmail.com>
2023-04-23 22:18:26 -0400

    style to card

--- components/card.js
+++ components/card.js
@@ -1,12 +1,12 @@
 export default function Card(props) {
-  function classes() {
+  const classes = function () {
     const bg = props.bgcolor ? " bg-" + props.bgcolor : "bg-dark";
     const txt = props.txtcolor ? " text-" + props.txtcolor : " text-white";
     return "container card mb-3 " + bg + txt;
-  }
+  };
 
   return (
-    <div className={`mt-5 ${classes()}`} style={{ width: "45rem" }}>
+    <div className={`mt-5 ${classes()}`}>
       <div className="card-header">{props.header}</div>
       <div className="card-body">
         {props.title && <h5 className="card-title">{props.title}</h5>}
