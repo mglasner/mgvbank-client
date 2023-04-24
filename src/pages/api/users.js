c3a8b4a92f1ba502e828d19fa62e9d8507767f21
mgv <mglasner10@gmail.com>
2023-04-24 16:27:36 -0400

    fix: users endpoint to work on GET and POST

--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -1,5 +1,17 @@
 export default async function handler(req, res) {
   const externalURL = process.env.EXTERNAL_API_URL;
-  const response = await fetch(`${externalURL}`);
-  res.send(await response.json());
+
+  if (req.method === "POST") {
+    const { name, email } = req.body;
+
+    const response = await fetch(`${externalURL}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, email }),
+    });
+    res.send(response.json());
+  } else {
+    const response = await fetch(`${externalURL}`);
+    res.send(await response.json());
+  }
 }
